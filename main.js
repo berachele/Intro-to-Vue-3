1cebe8abdd715c6fd480e8103e1b79a16a00da5c
Rachele Edwards <dot.com1472@gmail.com>
2020-11-16 16:46:57 -0700

    doing url for code challenge in L3

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const app = Vue.createApp({
         return {
             product: "Socks",
             description: "It's beginning to look a lot like...Warm-socks and Cuddle-with-Hot-Cocoa season! You'll love these long socks in either blue or green with the Vue.js logo on top.",
-            image: './assets/images/socks_green.jpg'
+            image: './assets/images/socks_green.jpg',
         }
     }
 })
